9fac9361c380b2977378f1a88fc295212d736ec9
Kushagra Mittal <kushagra.mittal@ucdconnect.ie>
2023-06-01 08:05:57 +0530

    Create Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,36 @@
+import modules.Number_Generator;
+import modules.Computer_Response;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+    {
+        // Variable Declaration
+        int guess, num, num_guesses;
+
+        // Generation of Random Number
+        num = Number_Generator.num();
+        System.out.println("A Random Number between 1-1000 (inclusive) has been Generated.");
+
+        // Set-up Input Scanner
+        Scanner scanner = new Scanner(System.in);
+
+        // First Guess
+        System.out.print("Guess a number: ");
+        guess = scanner.nextInt();
+        num_guesses = 1;
+        Computer_Response.response(num, guess);
+
+        while (guess != num)
+        {
+            System.out.print("Guess a number: ");
+            guess = scanner.nextInt();
+            num_guesses++;
+            Computer_Response.response(num, guess);
+
+        }
+        scanner.close();
+
+        System.out.println("You took only " + num_guesses + " guesses!");
     }
 }
